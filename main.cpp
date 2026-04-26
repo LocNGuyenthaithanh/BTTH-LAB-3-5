261c37b9327b605e4677e6de79ab8bebbd17c62e
LocNGuyenthaithanh <25521019@gm.uit.edu.vn>
2026-04-26 22:13:59 +0700

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,34 @@
-#include "cDaThuc.h"
 #include <iostream>
+#include "cDaThuc.h"
 using namespace std;
-int main(){
-cDaThuc p1,p2;
-p1.Nhap();
-p2.Nhap();
-p1.Xuat();
-p2.Xuat();
-float x; cin>>x;
-cout <<   p1.tinhGiaTri(x) << endl;
-    cout << p2.tinhGiaTri(x) << endl;
 
-    cDaThuc tong = p1.Cong(p2);
-    cout << "P1 + P2 = ";
+int main() {
+    cDaThuc p1, p2;
+
+    cout << "Nhap da thuc P1:\n";
+    p1.nhap();
+
+    cout << "\nNhap da thuc P2:\n";
+    p2.nhap();
+
+    cout << "\nP1 = ";
+    p1.xuat();
+
+    cout << "P2 = ";
+    p2.xuat();
+
+    float x;
+    cout << "\nNhap x: ";
+    cin >> x;
+
+    cout << "P1(" << x << ") = " << p1.tinhGiaTri(x) << endl;
+    cout << "P2(" << x << ") = " << p2.tinhGiaTri(x) << endl;
+
+    cDaThuc tong = p1.cong(p2);
+    cout << "\nP1 + P2 = ";
     tong.xuat();
 
-    cDaThuc hieu = p1.Tru(p2);
+    cDaThuc hieu = p1.tru(p2);
     cout << "P1 - P2 = ";
     hieu.xuat();
 
